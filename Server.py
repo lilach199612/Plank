b8ae6c5825fb47348bb7e47a4cfb009b317b0e9d
lilach199612 <lilach199612@gmail.com>
2021-11-09 14:45:58 +0200

    update the date of the current API

--- Server.py
+++ Server.py
@@ -2,21 +2,24 @@ import datetime
 
 from pip._vendor import requests
 from flask import Flask, request
-from flask import url_for
 
-API = requests.get("https://www.10bis.co.il/NextApi/GetRestaurantMenu?culture=en&uiCulture=en&restaurantId=19156&deliveryMethod=pickup").json()
-lastUpdate=datetime.date.today()
+API = requests.get(
+    "https://www.10bis.co.il/NextApi/GetRestaurantMenu?culture=en&uiCulture=en&restaurantId=19156&deliveryMethod=pickup").json()
+lastUpdate = datetime.date.today()
+
 
 def GET(categoryName, dishName="*"):
-    global API
-    if(lastUpdate!=datetime.date.today()):#update daily
-        API = requests.get("https://www.10bis.co.il/NextApi/GetRestaurantMenu?culture=en&uiCulture=en&restaurantId=19156&deliveryMethod=pickup").json()
-    categoryName=categoryName[0].upper()+categoryName[1:]
+    global API, lastUpdate
+    if (lastUpdate != datetime.date.today()):  # update daily
+        API = requests.get(
+            "https://www.10bis.co.il/NextApi/GetRestaurantMenu?culture=en&uiCulture=en&restaurantId=19156&deliveryMethod=pickup").json()
+        lastUpdate = datetime.date.today()
+    categoryName = categoryName[0].upper() + categoryName[1:]
     listOfThisCatagoryItems = []
     for catagory in API["Data"]["categoriesList"]:
         if catagory["categoryName"] == categoryName:
             listOfThisCatagoryItems = catagory
-            break;
+            break
 
     getItems = {}
     if dishName == "*":
@@ -26,7 +29,7 @@ def GET(categoryName, dishName="*"):
             curr["dishName"] = item["dishName"]
             curr["dishDescription"] = item["dishDescription"]
             curr["dishPrice"] = item["dishPrice"]
-            getItems[item["dishName"]]=curr
+            getItems[item["dishName"]] = curr
         return getItems
     else:
         for item in listOfThisCatagoryItems["dishList"]:
@@ -40,23 +43,24 @@ def GET(categoryName, dishName="*"):
         return dishName
 
 
-
-
 app = Flask(__name__)
+
+
 @app.route('/order', methods=['POST'])
 def POST():
-    orderDetail=request.json
+    orderDetail = request.json
     totalPrice = 0
     for catagory in orderDetail:
         for item in orderDetail[catagory]:
             totalPrice += GET(catagory, item)["dishPrice"]
     return {"price": totalPrice}
 
-@app.route("/<catagory>",methods=['GET'])
+
+@app.route("/<catagory>", methods=['GET'])
 def get(catagory):
     return GET(catagory)
 
-@app.route("/<catagory>/<id>",methods=['GET'])
-def getItem(catagory,id):
-    return GET(catagory,id)
 
+@app.route("/<catagory>/<id>", methods=['GET'])
+def getItem(catagory, id):
+    return GET(catagory, id)
---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+import Server
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_drinks(self):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
